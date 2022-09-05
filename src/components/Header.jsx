3c99022dbbe4d52e43cc74756b2f6d28bf22e7a1
PatrikVahlen <91195238+PatrikVahlen@users.noreply.github.com>
2022-09-05 16:42:06 +0200

    Added some CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import LinkedInIcon from "../LinkedIn_icon.svg.png";
 import FacebookIcon from "../facebook_icon.jpg";
 import GitHubIcon from "../github_icon.png";
@@ -7,18 +6,36 @@ import "../App.css";
 
 export default function Header() {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-sm">
-          <div>
-            <img src={LinkedInIcon} />
+    <div className="Header">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <a
+              href="https://www.linkedin.com/feed/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="icon" src={LinkedInIcon} alt="linkedIn Icon" />
+            </a>
           </div>
-        </div>
-        <div class="col-sm">
-          <img src={GitHubIcon} />
-        </div>
-        <div class="col-sm">
-          <img src={FacebookIcon} />
+          <div class="col-sm">
+            <a
+              href="https://github.com/PatrikVahlen/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="icon" src={GitHubIcon} alt="GitHub Icon" />
+            </a>
+          </div>
+          <div class="col-sm">
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="icon" src={FacebookIcon} alt="Facebook Icon" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
